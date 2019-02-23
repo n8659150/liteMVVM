181b895958aebb5b2bfd71761b36b963304418b3
n8659150 <jackli900421@hotmail.com>
2019-02-24 00:38:20 +0800

    add basic compile methods

--- compile.js
+++ compile.js
@@ -7,6 +7,16 @@ class Compile {
         }
     }
 
+    // toolbox functions
+    isElementNode (node) {
+        return node.nodeType === 1
+    }
+
+    isDirective (name) {
+        // starts with 'l-'
+        return name.includes('l-')
+    }
+
     // core functions
     nodeToFragment (el) {
         let fragment = document.createDocumentFragment();
@@ -19,21 +29,55 @@ class Compile {
         return fragment;
     }
 
+    compileElement(node) {
+        let nodeAttributes = node.attributes;
+        Array.from(nodeAttributes).forEach((attribute) => {
+            if (this.isDirective(attribute.name)) {
+                let result = attribute.value;
+                // TODO
+            }
+        })
+    }
+    
+    compileText(node) {
+        let text = node.textContent;
+        // get content between '{{}}'
+        let reg = /\{\P([^}]+)\}\}/g;
+        if (reg.test(text)) {
+            //TODO
+        }
+    }
     // compiles the element node and the text node
     compile (fragment) {
         let childNodes = fragment.childNodes;
         Array.from(childNodes).forEach((node) => {
             if (this.isElementNode(node)) {
-                console.log('this node is element node:', node);
+                this.compileElement(node);
                 this.compile(node)
             } else {
-                console.log('this node is text node:', node);
+                this.compileText(node);
             }
         })
     }
-    // toolbox functions
-    isElementNode (node) {
-        return node.nodeType === 1
+
+    CompileToolBox = {
+        text() {
+
+        },
+        model() {
+
+        },
+        updater: {
+            textUpdater (node, value) {
+                node.textContent = value;
+            },
+            modelUpdater (node, value) {
+                node.value = value
+            }
+        }
     }
+
+
+    
 
 }
